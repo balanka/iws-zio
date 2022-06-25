7173d43536354c66fbce7adb85c6b9d06d62bcba
balanka <batexy@gmail.com>
2022-06-25 14:48:04 +0200

    Added

---
+++ src/test/scala/com.kabasoft.iws/service/AccountServiceLiveSpec.scala
@@ -0,0 +1,37 @@
+package com.kabasoft.iws.service
+
+import com.kabasoft.iws.domain.common
+import com.kabasoft.iws.repository.common.AccountBuilder.{company, paccountId}
+import com.kabasoft.iws.repository.postgresql.PostgresContainer
+import com.kabasoft.iws.repository.{AccountRepositoryImpl, PacRepositoryImpl}
+import zio.ZLayer
+import zio.sql.ConnectionPool
+import zio.test.TestAspect._
+import zio.test._
+import java.time.{LocalDateTime, ZoneOffset}
+
+
+object AccountServiceLiveSpec extends ZIOSpecDefault {
+
+  val testServiceLayer = ZLayer.make[AccountService](
+    AccountRepositoryImpl.live,
+    PacRepositoryImpl.live,
+    AccountServiceImpl.live,
+    PostgresContainer.connectionPoolConfigLayer,
+    ConnectionPool.live,
+    PostgresContainer.createContainer
+  )
+
+  override def spec =
+    suite("Account service  test with postgres test container")(
+      test("Close an accounting  period") {
+        val previousYear  =  common.getYear(LocalDateTime.now().minusYears(1).toInstant(ZoneOffset.UTC))
+        val fromPeriod    = previousYear.toString.concat("01").toInt
+        val toPeriod    =  previousYear.toString.concat("12").toInt
+        for {
+          nrOfAccounts       <-AccountService.closePeriod(fromPeriod, toPeriod, paccountId, company)
+        } yield  assertTrue(nrOfAccounts == 1)
+      }
+    ).provideCustomLayerShared(testServiceLayer.orDie) @@ sequential
+}
+
