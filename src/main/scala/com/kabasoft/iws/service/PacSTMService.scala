409f32016e6a87b612adc1becde195957d300536
balanka <batexy@gmail.com>
2022-07-02 13:04:10 +0200

    Refactored

--- src/main/scala/com/kabasoft/iws/service/PacSTMService.scala
+++ src/main/scala/com/kabasoft/iws/service/PacSTMService.scala
@@ -15,7 +15,7 @@ object PacSTMService {
 
   def getDebit(account: TPeriodicAccountBalance): UIO[BigDecimal]  = account.debit.get.commit
   def getCredit(account: TPeriodicAccountBalance): UIO[BigDecimal] = account.credit.get.commit
-  def printLine(line: String): UIO[Unit]                           = ZIO.succeed(println(line))
+  def printLine(line: String): UIO[Unit]                           = Console.printLine(line).ignore
 
   def showBalance(from: TPeriodicAccountBalance, to: TPeriodicAccountBalance): UIO[Unit] =
     for {
@@ -31,22 +31,13 @@ object PacSTMService {
 
   def process: UIO[Unit] =
     for {
-      idebit   <- TRef.makeCommit(BigDecimal(0))
-      debit    <- TRef.makeCommit(BigDecimal(0))
-      credit   <- TRef.makeCommit(BigDecimal(0))
-      icredit  <- TRef.makeCommit(BigDecimal(0))
-      idebit2  <- TRef.makeCommit(BigDecimal(0))
-      debit2   <- TRef.makeCommit(BigDecimal(0))
-      credit2  <- TRef.makeCommit(BigDecimal(0))
-      icredit2 <- TRef.makeCommit(BigDecimal(0))
-
+      init   <- TRef.makeCommit(BigDecimal(0))
       from          =
-        TPeriodicAccountBalance("202205Account1", "Account1", 202205, idebit, debit, icredit, credit, "EUR", "1000")
+        TPeriodicAccountBalance("202205Account1", "Account1", 202205, init, init, init, init, "EUR", "1000")
       to            =
-        TPeriodicAccountBalance("202205Account2", "Account2", 202205, idebit2, debit2, icredit2, credit2, "EUR", "1000")
+        TPeriodicAccountBalance("202205Account2", "Account2", 202205, init, init, init, init, "EUR", "1000")
       _            <- showBalance(from, to)
       _            <- printLine(s"Before Transfering 10000  \n credited Acct  ${from}  \n debited Acct ${to}")
-      // _  <-ZIO.collectAllPar(ZIO.replicate(10000)(from.transfer(to, BigDecimal(1))))
       _            <- ZIO.collectAllPar(Chunk.fill(10000)(from.transfer(to, BigDecimal(1))))
       dbalanceFrom <- from.dbalance
       cbalanceFrom <- from.cbalance
