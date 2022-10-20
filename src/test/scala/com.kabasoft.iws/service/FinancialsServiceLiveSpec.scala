d89615120b8941738d13a8e65d74666bd86ba747
balanka <batexy@gmail.com>
2022-10-20 12:44:49 +0200

    Removed dead code

--- src/test/scala/com.kabasoft.iws/service/FinancialsServiceLiveSpec.scala
+++ src/test/scala/com.kabasoft.iws/service/FinancialsServiceLiveSpec.scala
@@ -39,15 +39,7 @@ object FinancialsServiceLiveSpec extends ZIOSpecDefault {
           balances       <-PacRepository.getBalances4Period(period, period, company).runCollect.map(_.toList)
         } yield {println("balances>>>"+balances);(assertTrue(oneRow == 3)&& assertTrue(postedRows == 10) && assertTrue(nrOfPacs == 1)
         && assertTrue(journalEntries == 2)&& assertTrue(oaccountEntry == 1)&& assertTrue(vatEntry == 1))}
-      },
-      /*test("getBalances4period for account imported  ") {
-        val period    =  common.getPeriod(Instant.now())
-        for {
-            nrOfPacs       <-PacRepository.getBalances4Period(period, period, company).runCollect.map(_.toList)
-        } yield {println("nrOfPacs>>>"+nrOfPacs); assertTrue(nrOfPacs.size ==1)}
       }
-
-       */
     ).provideLayerShared(testServiceLayer.orDie) @@ sequential
 }
 
