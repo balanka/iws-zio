06d4e240ae433550ab5cae873408a0b052624d12
balanka <batexy@gmail.com>
2023-02-17 23:14:39 +0100

    Bug fixing

--- src/test/scala/com.kabasoft.iws/domain/CustomerBuilder.scala
+++ src/test/scala/com.kabasoft.iws/domain/CustomerBuilder.scala
@@ -21,7 +21,7 @@ object CustomerBuilder {
   val customer2 = Customer(customerId2, nameCustomer2, "KABA Soft GmbH", "Universitaet Str 2", "33615", "Bielefeld", "NRW", "DE",
       "0521-4711631", "0521-4711641", "info@fabasoft.de", accountRecvId, incaccountId, IbanCustomer2, vataccountId, 1000)
 
-  val dummy = Customer(dummyId, dummyNname, dummyNname, dummyNname, dummyNname, dummyNname, dummyNname, dummyNname,"DE",
+  val dummy = Customer(dummyId, dummyNname, dummyNname, dummyNname, dummyNname, dummyNname, dummyNname, "DE", dummyNname,
        "dummy@dummy.com", "1215", "111111", "DEddddddddddddddddommy", "v5", "1000", Customer.MODELID, vtime, vtime, vtime)
   val customers = List( customer1, customer2 )
 
