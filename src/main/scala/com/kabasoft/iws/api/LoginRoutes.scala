ebafa5036dc6aaeeeb2eed1918c115ffe67388c2
balanka <batexy@gmail.com>
2023-07-28 12:26:11 +0200

    Fixed bug related to writting the journal

--- src/main/scala/com/kabasoft/iws/api/LoginRoutes.scala
+++ src/main/scala/com/kabasoft/iws/api/LoginRoutes.scala
@@ -16,22 +16,15 @@ import java.time.Clock
 
 object LoginRoutes {
 
-  // Secret Authentication key
   val SECRET_KEY = "secretKey"
-
   implicit val clock: Clock = Clock.systemUTC
 
-  // Helper to encode the JWT token
   def jwtEncode(username: String, liveSpan: Long): String = {
-    // val json = s"""{"user": "${username}"}"""
     val json  = s"""{$username}"""
-    val claim = JwtClaim {
-      json
-    }.issuedNow.expiresIn(liveSpan)
+    val claim = JwtClaim {json}.issuedNow.expiresIn(liveSpan)
     Jwt.encode(claim, SECRET_KEY, JwtAlgorithm.HS512)
   }
 
-  // Helper to decode the JWT token
   def jwtDecode(token: String): Option[JwtClaim] = {
     val result = Jwt.decode(token, SECRET_KEY, Seq(JwtAlgorithm.HS512)).toOption
     println(s"jwtDecoded  $result")
@@ -42,7 +35,6 @@ object LoginRoutes {
   def appLogin = Http.collectZIO[Request] {
     case req@Method.POST -> Root / "users" / "login" =>
       for {
-
         body <- req.body.asString
           .flatMap(request =>
             ZIO
@@ -50,29 +42,22 @@ object LoginRoutes {
               .mapError(e => RepositoryError(e))
               .tapError(e => ZIO.logInfo(s"Unparseable body ${e}"))
           )
-          // .either
           .flatMap (checkLogin)
       } yield body
   }
 
-  //def loginAPI = Endpoint.post("users" / "login").in[LoginRequest].out[User].outError[RepositoryError](Status.InternalServerError)
-  //val loginEndpoint = loginAPI.implement {
- //   loginRequest => checkLogin(loginRequest).mapError(e => RepositoryError(e.getMessage))
-     // Response.json(user.toJson).addHeader(Header("authorization", token)).addHeader(Header("Origin", "localhost:3000"))
- // }
   private def checkLogin(loginRequest: LoginRequest): ZIO[UserRepository, RepositoryError, Response] = for {
     _ <- ZIO.logInfo(s"checkLogin >>>>>>")
-    //r <- UserRepository.list(loginRequest.company).runCollect.map(_.toList)
-    r <- UserRepository.list("1000").runCollect.map(_.toList)
+    //_ <- ZIO.logInfo(s"pwd >>>>>> ${jwtEncode(loginRequest.password,1000000)}")
+    r <- UserRepository.list(loginRequest.company).runCollect.map(_.toList)
     user = r.find(_.userName.equals(loginRequest.userName)).getOrElse(DummyUser)
     content   = jwtDecode(user.hash).toList.head.content.replace("{","").replace("}","")
-    _ <- ZIO.logInfo(s"user >>>>>> ${user}")
-    //_ <- ZIO.logInfo(s"pwd >>>>>> ${jwtEncode(loginRequest.password,1000000)}")
-    _ <- ZIO.logInfo(s"password >>>>>> ${content} >>>>>   ${content.substring(1, content.length - 1)}")
+   // _ <- ZIO.logInfo(s"user >>>>>> ${user}")
+   // _ <- ZIO.logInfo(s"password >>>>>> ${content} >>>>>   ${content.substring(1, content.length - 1)}")
   } yield {
 
     println(s"content >>>>>> $content")
-    val pwd       = content//.substring(1, content.length - 1).replaceAll("\"", "")
+    val pwd       = content
     val pwdR      = loginRequest.password
     val usernameR = loginRequest.userName
     val username  = user.userName
@@ -80,14 +65,13 @@ object LoginRoutes {
     if (check) {
       val json = s"""{"${loginRequest.password}"}"""
       val token = jwtEncode(json, 1000000)
-      Response.json(user.toJson).addHeader(Custom("authorization", token))//.addHeader(Custom("Origin", "http://Mac-Studio.fritz.box:3000"))
+      Response.json(user.toJson).addHeader(Custom("authorization", token))
         .addHeader(Custom("Origin", "http://localhost:3000"))
         .addHeader(Custom("Access-Control-Allow-Origin", "*"))
     } else {
       Response.text("Invalid  user name or password "
         + loginRequest.userName + "/"
         + loginRequest.password).withStatus(Status.Unauthorized)
-
     }
   }
 }
--- src/main/scala/com/kabasoft/iws/service/FinancialsServiceImpl.scala
+++ src/main/scala/com/kabasoft/iws/service/FinancialsServiceImpl.scala
@@ -2,19 +2,14 @@ package com.kabasoft.iws.service
 
 import com.kabasoft.iws.domain.AppError.RepositoryError
 import com.kabasoft.iws.domain.common._
-import com.kabasoft.iws.domain.{FinancialsTransaction, FinancialsTransactionDetails, Journal, PeriodicAccountBalance, TPeriodicAccountBalance, common}
+import com.kabasoft.iws.domain.{FinancialsTransaction, FinancialsTransactionDetails, Journal, PeriodicAccountBalance,  common}
 import com.kabasoft.iws.repository.{JournalRepository, PacRepository, PostTransactionRepository, TransactionRepository}
 import zio._
 
-
-
 final class FinancialsServiceImpl(pacRepo: PacRepository, ftrRepo: TransactionRepository, journalRepo: JournalRepository, repository4PostingTransaction:PostTransactionRepository) extends FinancialsService {
-  type DPAC = TPeriodicAccountBalance
-  type FTDetails = FinancialsTransactionDetails
 
   override def create(model: FinancialsTransaction): ZIO[Any, RepositoryError, Int] =
     ftrRepo.create(model)
-
 
   override def create(items: List[FinancialsTransaction]): ZIO[Any, RepositoryError, Int] =
     ftrRepo.create(items)
@@ -59,12 +54,12 @@ final class FinancialsServiceImpl(pacRepo: PacRepository, ftrRepo: TransactionRe
       _ <- ZIO.logInfo(s" pacs ${pacs}")
       newRecords = PeriodicAccountBalance.create(model).filterNot(pac => pacs.map(_.id).contains(pac.id))
         .groupBy(_.id) map { case (_, v) =>
-        common.reduce(v, PeriodicAccountBalance.dummy) // .copy(id = k)
+        common.reduce(v, PeriodicAccountBalance.dummy) 
       }
       _ <- ZIO.logInfo(s" newRecords ${newRecords}")
       oldPacs = updatePac(model, pacs)
       _ <- ZIO.logInfo(s" oldPacs ${oldPacs}")
-      journalEntries = makeJournal(model, newRecords.toList ++ pacs)
+      journalEntries = makeJournal(model, newRecords.toList ++ oldPacs)
       _ <- ZIO.logInfo(s" journalEntries ${journalEntries}")
       post <- repository4PostingTransaction.post(List(model), newRecords.toList, oldPacs, journalEntries)
 
@@ -85,11 +80,10 @@ final class FinancialsServiceImpl(pacRepo: PacRepository, ftrRepo: TransactionRe
     val r0:List[PeriodicAccountBalance] = groupById(PeriodicAccountBalance.create(model))
     val r:List[PeriodicAccountBalance] = r0.map { pac =>
       pacs.find(_.id ==  pac.id).map(pac_ => pac_.copy(idebit = pac_.idebit.add(pac.idebit), debit = pac_.debit.add(pac.debit)
-        , icredit = pac_.icredit.add(pac.icredit), credit = pac_.credit.add(pac.credit)))
-      .getOrElse(PeriodicAccountBalance.dummy)
-    }
-      //TPeriodicAccountBalance.debitAndCreditAllX(pacs, poacs, line.amount)
-      val result =  groupById(r)
+                                                    , icredit = pac_.icredit.add(pac.icredit), credit = pac_.credit.add(pac.credit)))
+      .getOrElse(pac)
+    }.filterNot(_.id ==PeriodicAccountBalance.dummy.id)
+   val result =  if(pacs.isEmpty) r0 else groupById(r)
     result
   }
 
@@ -101,17 +95,22 @@ final class FinancialsServiceImpl(pacRepo: PacRepository, ftrRepo: TransactionRe
     r0
   }
 
+  def findOrBuildPac(pacId: String, period_ : Int, pacList: List[PeriodicAccountBalance]) =
+    pacList.find(pac_ => pac_.id == pacId).getOrElse(PeriodicAccountBalance.dummy.copy(id = pacId, period = period_))
   private def makeJournal(model: FinancialsTransaction, pacList: List[PeriodicAccountBalance]): List[Journal] = for {
     journal <- model.lines.flatMap { line =>
-                 val pac: Option[PeriodicAccountBalance]  = pacList.find(pac_ => pac_.id == buildPacId(model.getPeriod, line.account))
-                 val poac: Option[PeriodicAccountBalance] = pacList.find(poac_ => poac_.id == buildPacId(model.getPeriod, line.oaccount))
-                 val jou1                                 = pac.map(buildJournalEntries(model, line, _, line.account, line.oaccount, line.side))
-                 val jou2                                 = poac.map(buildJournalEntries(model, line, _, line.oaccount, line.account, !line.side))
-                 List(jou1, jou2).flatten
-               }
+       val pacId  = buildPacId(model.getPeriod, line.account)
+       val poacId = buildPacId(model.getPeriod, line.oaccount)
+       val pac    = findOrBuildPac(pacId, model.getPeriod, pacList)
+       val poac   = findOrBuildPac(poacId, model.getPeriod, pacList)
+       val jou1   = buildJournalEntries(model, line, pac, line.account, line.oaccount, line.side)
+       val jou2   = buildJournalEntries(model, line, poac, line.oaccount, line.account, !line.side)
+       List(jou1, jou2)
+       }
   } yield journal
 
-  private def buildJournalEntries(model: FinancialsTransaction, line: FTDetails, pac: PeriodicAccountBalance, account: String, oaccount: String,side:Boolean) =
+  private def buildJournalEntries(model: FinancialsTransaction, line: FinancialsTransactionDetails,
+                                  pac: PeriodicAccountBalance, account: String, oaccount: String,side:Boolean) =
     Journal(
       -1,
       model.id,
